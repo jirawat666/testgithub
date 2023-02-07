d079086aa9b5fe89433851f8bef03c2444bd9004
Jirawat666 <saisawan.ji@gmail.com>
2023-02-07 14:19:21 +0700

    ###Interrupt function

--- demo.py
+++ demo.py
@@ -1,2 +1,26 @@
 print("Test1")
 print("Sometimese1")
+from threading import Event
+
+exit = Event()
+
+def main():
+    while not exit.is_set():
+      # do_my_thing()
+      print(1)
+      exit.wait(1)
+
+    print("All done!")
+    # perform any cleanup here
+
+def quit(signo, _frame):
+    print("Interrupted by %d, shutting down" % signo)
+    exit.set()
+
+if __name__ == '__main__':
+
+    import signal
+    for sig in ('TERM','INT'):
+        signal.signal(getattr(signal, 'SIG'+sig), quit);
+
+    main()
